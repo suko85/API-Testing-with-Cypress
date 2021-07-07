de18ce2c9d89f659bee8f0db0aad098cbeac1ea8
Suko <sukoggu@gmail.com>
2021-07-07 12:17:33 -0300

    crossbrowser testing

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -4,7 +4,7 @@ describe('Test log out', () =>{
     beforeEach('login to the app', () => {
     cy.loginToApplication()
     })
-
+  
    it('verify use can log out successfully', () =>{
     cy.contains('Settings').click()
     cy.contains('Or click here to logout').click()
